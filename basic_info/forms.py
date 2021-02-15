1de1c839e48345d4446205dabc48fb194d25c178
techmirfz <tech.mirfz@gmail.com>
2021-02-15 15:34:28 +0800

    edit basicinfoedit form

--- basic_info/forms.py
+++ basic_info/forms.py
@@ -4,17 +4,22 @@ from .models import BasicInfo
 class BasicInfoForm(forms.ModelForm):
 	class Meta:
 		model = BasicInfo
-		fields = ['afsn', 'firstname', 'middlename', 'lastname', 'extensionname', 'gender', 'classification', 'rank', 'assign', 'source']
+		fields = ['afsn', 'rank', 'firstname', 'middlename', 'lastname', 'extensionname', 'gender', 'classification', 'assign', 'source', 'dateentersvc', 'datecompret']
 
 
 		widgets = {
             'afsn': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'AFSN'}),
+            'rank': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'RANK'}),
             'firstname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'FIRST NAME'}),
             'middlename': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'MIDDLE NAME'}),
             'lastname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'LAST NAME'}),
-            'category': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CATEGORY'}),
-            'rank': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'RANK'}),
-            'assign': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'UNIT'}),
-		   
-		    }
+            'extensionname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'EXTENSION NAME'}),
+            'gender': forms.Select(attrs={'class': 'form-control', 'placeholder': 'GENDER'}),
+            'classification': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CLASSIFICATION'}),
+            'assign': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CURRENT UNIT'}),
+		'source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'SOURCE OF COMMISSION/ENLISTMENT'}), 
+            'dateentersvc': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'DATE ENTERED SERVICE'}),
+            'datecompret': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'DATE OF COMPULSORY RETIREMENT'}), 
+		}
 
+           
